6dbc3a7a4dd3d499b99e0138f85aaa4d02338821
Anugrah <pnanugrah@gmail.com>
2024-10-31 22:57:21 -0700

    added stylua file

---
+++ README.md
@@ -0,0 +1,2 @@
+
+- Add option to enable a popup confirming whether or not a new file should be created if it doesn't already exist
--- lua/better_gf.lua
+++ lua/better_gf.lua
@@ -3,6 +3,7 @@ local M = {}
 M.setup = function ()
   vim.keymap.set('n','gf', ':e <cfile><CR>',{} )
 end
- -- ./test.c
+
+
 
 return M
