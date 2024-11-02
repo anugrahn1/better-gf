7220c7acded78c4942c5aa677cf10da2402bb5f4
Anugrah <pnanugrah@gmail.com>
2024-11-02 00:27:22 -0700

    reworked how gf functions

--- lua/better_gf.lua
+++ lua/better_gf.lua
@@ -1,13 +1,23 @@
 local M = {}
 
 M.setup = function()
-  vim.keymap.set('n', 'gf', ':e <cfile><CR>', {})
+  vim.api.nvim_create_user_command("BetterGF", M.gf_replace, {})
 end
 
+-- TODO: make using picker optional
+M.gf_replace = function()
+  if pcall(vim.cmd.normal, 'gf') then -- if file already exists, continues like normal gf
+  else
+    M.open_picker() -- if file doesn't exist, open picker
+  end
+end
+
+-- TODO: use word under cursor when pressing keybind and pass it to menu
 M.open_picker = function()
   local menu = require 'test'
   menu:mount()
 end
 
-M.open_picker()
-return
+-- M.open_picker()
+
+return M
